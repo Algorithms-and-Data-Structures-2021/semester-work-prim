58b3346d8de634ada224a916fb9efc59aed21beb
Olga <terenteva2002@yandex.ru>
2021-05-17 22:31:54 +0300

    code success

--- include/data_structure.hpp
+++ include/data_structure.hpp
@@ -1,27 +1,36 @@
 #pragma once
 
-// Заголовочный файл с объявлением структуры данных
+#include <iostream>
 
-namespace itis {
+using namespace std;
 
-  // Tip 1: объявите здесь необходимые структуры, функции, константы и прочее
+//Edge structure
+struct edge {
+  int v_start;
+  int v_end;
+  int weight;
+};
 
-  // Пример: объявление константы времени компиляции в заголовочном файле
-  inline constexpr auto kStringConstant = "Hello, stranger!";
+struct node {
+  int key;
+  int degree;
+  node* parent;
+  node* child;
+  node* sibling;
+  int i;
+};
 
-  // Пример: объявление структуры с полями и методами
-  struct MyStructure {
-   public:
-    int size_{0};
-    int capacity_{0};
-    int* data_{nullptr};
+struct vertex {
+  vertex() {
+    visited = false;
+    selected = 0;
+    inHeap = 0;
+    i = 0;
+  }
 
-    // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
-    // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
-
-    int size() const {
-      return size_;
-    }
-  };
-
-}  // namespace itis
+  bool visited;
+  edge* selected;
+  node* inHeap;
+  vector<edge*> connect;
+  int i;
+};
